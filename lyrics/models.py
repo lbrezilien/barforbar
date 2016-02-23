4bd38b3e3aaf234029da7f069c22fe33a69d765e
lbrezilien <lbrezilien@gmail.com>
2016-02-23 03:01:18 -0500

    configures urls for root and users

--- lyrics/models.py
+++ lyrics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Lyric(models.Model):
+    user = models.ForeignKey(User)
+    artist = models.CharField(max_length = 300)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
+
+
+# Should I split up my models? Not sure
--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Contact(models.Model):
-    phone =models.CharField(max_length = 13)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    zipcode = models.CharField(max_length=13)
 
 class About(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length = 300)
     genre_interests = models.CharField(max_length=100)
     why_quote = models.DateField()
     school= models.CharField(max_length=50)
     educational_focus = models.CharField(max_length=100)
+    phone =models.CharField(max_length = 13)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=13)
